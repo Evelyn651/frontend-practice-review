e3ae2175676c36a04d8d75bb361ac50c4924fb5b
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-08-20 22:03:13 -0500

    feat: working with mathematical assignments to change values

--- js/main.js
+++ js/main.js
@@ -14,3 +14,20 @@ console.log(entree);
 // entree = "Tacos"; throws an error message because const's cannot be reassigned values
 
 // const testing; const's cannot be declared without a value
+
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+//using mathematical assignments to change values
+
+levelUp += 5;
+powerLevel -= 100;
+multiplyMe *= 11;
+quarterMe /= 4;
+
+console.log("The value of levelUp after mathematical assignment: ", levelUp);
+console.log("The value of powerLevel after mathematical assignment: ", powerLevel);
+console.log("The value of multipyMe after mathematical assignment: ", multiplyMe);
+console.log("The value of quarterMe after mathematical assignment: ", quarterMe);
