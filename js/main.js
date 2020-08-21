e02e407b0904f50038844375f7dfe73a471b080f
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-08-20 21:56:46 -0500

    feat: working with const and reviewing how it works

--- js/main.js
+++ js/main.js
@@ -7,3 +7,10 @@ console.log(numOfSlices);
 let changeMe = true;
 changeMe = false;
 console.log(changeMe);
+
+const entree = "Enchiladas";
+console.log(entree);
+
+// entree = "Tacos"; throws an error message because const's cannot be reassigned values
+
+// const testing; const's cannot be declared without a value
