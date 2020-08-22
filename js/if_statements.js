58760b0f0439ced0bbad6248f3058de315f441cd
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-08-21 22:22:36 -0500

    feat: practicing with truthy and falsy

--- js/if_statements.js
+++ js/if_statements.js
@@ -24,3 +24,20 @@ if(mood === "sleepy" && tirednessLevel > 8){
 
 //with && it prints not bet time yet
 //with || it prints time to sleep
+
+let wordCount = 1;
+
+if (wordCount) {
+    console.log("Great! You've started your work!");
+} else {
+    console.log('Better get to work!');
+}
+
+
+let favoritePhrase = '';
+
+if (favoritePhrase) {
+    console.log("This string doesn't seem to be empty.");
+} else {
+    console.log('This string is definitely empty.');
+}
