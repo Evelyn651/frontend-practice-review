e30304de2e956ced5354f8ae2311617fbb9443c4
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-08-22 23:32:38 -0500

    feat: practice with ternary operator

--- js/if_statements.js
+++ js/if_statements.js
@@ -54,3 +54,8 @@ let isLocked = false;
 
 isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the' +
     ' door.');
+
+let isCorrect = true;
+
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+
