f34abb508e9c0cbed814c99b0dc3a0ac2f2d1fbc
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-08-24 23:04:31 -0500

    feat: more practice with tenary operator

--- js/if_statements.js
+++ js/if_statements.js
@@ -59,3 +59,6 @@ let isCorrect = true;
 
 isCorrect ? console.log('Correct!') : console.log('Incorrect!');
 
+let favoritePhrase = "Love That!";
+
+favoritePhrase === "Love That!" ? console.log("I love that!") : console.log("I don't love that!");
