730d173398bd726ad921142c2154bf4e09795eba
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-08-25 00:18:56 -0500

    feat: more practice with global scope variables

--- js/BlocksAndScopes.js
+++ js/BlocksAndScopes.js
@@ -13,3 +13,12 @@ let stars = 'North Star';
 function callMyNightSky(){
     return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
 }
+
+//block scope practice
+function logVisibleLightWaves(){
+    const lightWaves = 'Moonlight';
+    console.log(lightWaves);
+}
+
+logVisibleLightWaves();
+console.log(lightWaves);//not accessible because it was declared within the function scope
